73b69146afade81e1f8d205baf1ea8aec4fb76da
timmichanga13 <timmichanga13@gmail.com>
2021-09-12 17:07:58 -0400

    routing practice

--- react/react_routing/routing_practice/src/App.js
+++ react/react_routing/routing_practice/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { useParams } from "react-router";
+
+const Home = (props) => {
+  return (
+    <h1>Welcome!</h1>
+  );
+}
+
+const Mega = (props) => {
+  const { mega, textColor, backgroundColor } = useParams();
+
+  return (
+    <div style={{ background: backgroundColor }}>
+      <p style={{ color: textColor }}>The {isNaN(mega) ? 'word' : 'number'} you typed is: {mega}.</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <h1>Routing Practice</h1>
+      <p>
+        <Link to="/home">Home</Link>
+      </p>
+      <Switch>
+        {/* this is what is typed in teh url */}
+        <Route path="/home">
+          {/* this is the function that is called (from above/components folder) */}
+          <Home />
+        </Route>
+        {/* basic route, exact to route here ifd it has teh exact match */}
+        <Route exact path="/:mega">
+          <Mega />
+        </Route>
+        
+        {/* remove exact if you want to make it flexible */}
+        <Route exact path="/:mega/:textColor">
+          <Mega />
+        </Route>
+
+        {/* remove exact if you want to make it flexible */}
+        <Route exact path="/:mega/:textColor/:backgroundColor">
+          <Mega />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
   );
 }
 
