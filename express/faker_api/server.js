a71373ce85e2d9d045a195cea5cfcbf2a4c6dccc
timmichanga13 <timmichanga13@gmail.com>
2021-09-15 13:13:33 -0400

    faker api assignment

--- express/faker_api/server.js
+++ express/faker_api/server.js
@@ -2,27 +2,20 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-const faker = require('faker');
+var faker = require('faker');
 
 
 // make sure these lines are above any app.get or app.post code blocks
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// we can hard code some users like this
-// later on we will want to store users in a database
-const users = [
-    { firstName: "Reimu", lastName: "Hakurei" },
-    { firstName: "Marisa", lastName: "Kirisame" },
-    { firstName: "Sanae", lastName: "Kochiya" },
-    { firstName: "Sakuya", lastName: "Izayoi" },
-    { firstName: "Momiji", lastName: "Inubashiri" }
-];
 
+const users = [];
+const companies = [];
 
 class User {
     constructor(id) {
-        this._id = id;
+        this._id = faker.datatype.uuid();
         this.firstName = faker.name.firstName();
         this.lastName = faker.name.lastName();
         this.phoneNumber = faker.phone.phoneNumber();
@@ -33,78 +26,38 @@ class User {
 
 class Company {
     constructor(id) {
-        this._id = id;
+        this._id = faker.datatype.uuid();
         this.name = faker.company.companyName();
         this.address = {
             street: faker.address.streetName(),
             city: faker.address.city(),
             state: faker.address.state(),
-            zipCode: faker.address.zipCodeByState(this.adress[state]),
+            zipCode: faker.address.zipCodeByState(),
             country: faker.address.country()
         }
     }
 }
 
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello World" });
-});
-
-
 app.get("/api/users", (req, res) => {
     res.json(users);
 });
 
-
-app.post("/api/users", (req, res) => {
-    // req.body will contain the form data from Postman or from React
-    console.log(req.body);
-    // we can push it into the users array for now...
-    // later on this will be inserted into a database
-    users.push(req.body);
-    // we always need to respond with something
-    res.json({ status: "ok" });
-});
-
-app.post("/api/users/new", (req, res) => {
-    console.log(req.body);
+app.get("/api/users/new", (req, res) => {
     let newUser = new User();
     users.push(newUser);
-    res.json({status: "ok"});
+    res.json({data: newUser});
 });
 
-
-// if we want to get a user with a specific id, we can make the id a part of the url
-// be sure to preface the id variable with a `:` colon
-app.get("/api/users/:id", (req, res) => {
-    // we can get this `id` variable from req.params
-    console.log(req.params.id);
-    // assuming this id is the index of the users array we could return one user this way
-    res.json(users[req.params.id]);
+app.get("/api/companies", (req, res) => {
+    res.json(companies);
 });
 
-
-app.put("/api/users/:id", (req, res) => {
-    // we can get this `id` variable from req.params
-    const id = req.params.id;
-    // assuming this id is the index of the users array we can replace the user like so
-    users[id] = req.body;
-    // we always need to respond with something
-    res.json({ status: "ok" });
+app.get("/api/companies/new", (req, res) => {
+    let newComp = new Company();
+    companies.push(newComp);
+    res.json({data: newComp});
 });
-
-
-app.delete("/api/users/:id", (req, res) => {
-    // we can get this `id` variable from req.params
-    const id = req.params.id;
-    // assuming this id is the index of the users array we can remove the user like so
-    users.splice(id, 1);
-    // we always need to respond with something
-    res.json({ status: "ok" });
-});
-
-
-
 
 // this needs to below the other code blocks
 app.listen(port, () => console.log(`Listening on port: ${port}`));
